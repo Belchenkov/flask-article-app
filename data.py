51374b2f480eb64a35de1514956cff8f55bf5ceb
Aleksey Belchenkov <u608110@gmail.com>
2020-04-08 17:23:46 +0300

    List Articles & Single Article Pages

---
+++ data.py
@@ -0,0 +1,33 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda pariatur, inventore alias, odio tempore maxime tempora reiciendis adipisci fugiat, sunt modi? Vitae quia tempora quos, dolorem nostrum corrupti. Mollitia, obcaecati?',
+            'author': 'Aleksey Belchenkov',
+            'create_date': '04-08-2020'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda pariatur, inventore alias, odio tempore maxime tempora reiciendis adipisci fugiat, sunt modi? Vitae quia tempora quos, dolorem nostrum corrupti. Mollitia, obcaecati?',
+            'author': 'Aleksey Belchenkov',
+            'create_date': '04-10-2020'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda pariatur, inventore alias, odio tempore maxime tempora reiciendis adipisci fugiat, sunt modi? Vitae quia tempora quos, dolorem nostrum corrupti. Mollitia, obcaecati?',
+            'author': 'Aleksey Belchenkov',
+            'create_date': '04-11-2020'
+        },
+        {
+            'id': 4,
+            'title': 'Article Four',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda pariatur, inventore alias, odio tempore maxime tempora reiciendis adipisci fugiat, sunt modi? Vitae quia tempora quos, dolorem nostrum corrupti. Mollitia, obcaecati?',
+            'author': 'Aleksey Belchenkov',
+            'create_date': '04-15-2020'
+        }
+    ]
+
+    return articles
